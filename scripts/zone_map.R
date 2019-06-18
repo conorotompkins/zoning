e7cd89f2a9e19bf81a2a20266d8051538a8614cf
Conor Tompkins <conorotompkins@gmail.com>
2019-06-18 22:18:14 +0000

    updated script for image

--- scripts/zone_map.R
+++ scripts/zone_map.R
@@ -12,8 +12,12 @@ df <- shapefile %>%
          attached = str_detect(full_zonin, "ATTACHED"),
          type = case_when(residential == TRUE & single_unit == TRUE & attached == FALSE ~ "Single-unit detached residential",
                           residential == TRUE & single_unit == FALSE | attached == TRUE ~ "Other residential",
-                          full_zonin == "EDUCATIONAL/MEDICAL INSTITUTION" ~ "Edu/Medical",
+                          full_zonin == "EDUCATIONAL/MEDICAL INSTITUTION" ~ "Educational/Medical",
                           residential == FALSE ~ "Other non-residential"),
+         type = factor(type, levels = c("Single-unit detached residential", 
+                                        "Other residential",
+                                        "Educational/Medical",
+                                        "Other non-residential")),
          alpha_flag = type == "Single-unit detached residential")
 
 test <- df %>% 
@@ -21,15 +25,25 @@ test <- df %>%
   st_drop_geometry()
 test
 
-df %>% 
-  ggplot(aes(fill = type, alpha = alpha_flag)) +
-  geom_sf(color = NA) +
-  scale_alpha_discrete(range = c(.3, 1))+
-  guides(alpha = FALSE) +
-  theme_void()
-
-df %>% 
+pct_single <- df %>% 
   st_drop_geometry() %>% 
   filter(residential == TRUE) %>% 
-  summarize(pct_single_unit_detached_residential = mean(type == "Single-unit detached residential"))
+  summarize(pct_single_unit_detached_residential = mean(type == "Single-unit detached residential")) %>% 
+  mutate(pct_single_unit_detached_residential = round(pct_single_unit_detached_residential, digits = 3)) %>% 
+  pull(pct_single_unit_detached_residential)
+
+zone_map <- df %>% 
+  ggplot(aes(fill = type)) +
+    geom_sf() +
+    scale_fill_manual("Zone type",
+                      values = c("green", "blue", "yellow", "light grey")) +
+    labs(title = "34% of residential zoned land area is single-family detached",
+         subtitle = "CIty of Pittsburgh zoning",
+         caption = "@conor_tompkins, data from WPRDC") +
+    theme_void()
+zone_map
+
+ggsave("output/zone_map.png", zone_map)
+
+
   
